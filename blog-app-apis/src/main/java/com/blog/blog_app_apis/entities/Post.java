712b8eefcc3106fa7848c370f1571817e60af489
Lokesh <lokeshchoudhary2518@gmail.com>
2024-07-27 16:08:34 +0530

    US30405:- minor fixes

--- blog-app-apis/src/main/java/com/blog/blog_app_apis/entities/Post.java
+++ blog-app-apis/src/main/java/com/blog/blog_app_apis/entities/Post.java
@@ -2,26 +2,118 @@ package com.blog.blog_app_apis.entities;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="post")
+@Table(name = "post")
 public class Post {
-  
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer postId;
-	
+
+	@Column(name = "post_title", length = 100, nullable = false)
 	private String title;
-	
+
+	@Column(length = 10000)
 	private String content;
-	
+
 	private String imageName;
-	
+
 	private Date addedDate;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "catagory_id")
+	private Catagory catagory;
+
+	@ManyToOne
+	private User user;
+
+	// Getter and Setter for postId
+	public Integer getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+
+	// Getter and Setter for title
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	// Getter and Setter for content
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	// Getter and Setter for imageName
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	// Getter and Setter for addedDate
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	// Getter and Setter for catagory
+	public Catagory getCatagory() {
+		return catagory;
+	}
+
+	public void setCatagory(Catagory catagory) {
+		this.catagory = catagory;
+	}
+
+	// Getter and Setter for user
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Post(Integer postId, String title, String content, String imageName, Date addedDate, Catagory catagory,
+			User user) {
+		super();
+		this.postId = postId;
+		this.title = title;
+		this.content = content;
+		this.imageName = imageName;
+		this.addedDate = addedDate;
+		this.catagory = catagory;
+		this.user = user;
+	}
+
+	public Post() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
